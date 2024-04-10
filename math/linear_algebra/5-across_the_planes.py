ff860d125ec527d30457d182c03c843c030c3d40
gastonperego <gastonperego@gmail.com>
2024-04-10 22:02:25 +0000

    feat: task 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -8,7 +8,7 @@ def add_matrices2D(mat1, mat2):
     """
         Adds 2 matrices
     """
-    
+
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
 
@@ -18,5 +18,5 @@ def add_matrices2D(mat1, mat2):
         for j in range(len(mat1[0])):
             array.append(mat1[i][j] + mat2[i][j])
         sum.append(array)
-    
+
     return sum
