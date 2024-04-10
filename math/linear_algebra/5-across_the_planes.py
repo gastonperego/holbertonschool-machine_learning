1ddc9492f78cc392086373e578dc6364d0a0c462
gastonperego <gastonperego@gmail.com>
2024-04-10 22:01:47 +0000

    feat: task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Task 5
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+        Adds 2 matrices
+    """
+    
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+
+    sum = []
+    for i in range(len(mat1)):
+        array = []
+        for j in range(len(mat1[0])):
+            array.append(mat1[i][j] + mat2[i][j])
+        sum.append(array)
+    
+    return sum
