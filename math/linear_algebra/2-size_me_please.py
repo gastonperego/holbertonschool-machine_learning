965a2831845f1a4d957fee6860a50f8dbdee1b3f
gastonperego <gastonperego@gmail.com>
2024-04-10 16:38:28 +0000

    fix: pycodestyle and documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    """
+        Function that returns the shape of a given matrix
+    """
     array = []
-    
+
     recursive(matrix, array)
     return array
 
 
 def recursive(matrix, array):
+    """
+        Recursive function that appends the length of the nested arrays
+    """
     if not isinstance(matrix, int):
         array.append(len(matrix))
-        recursive(matrix[0], array) 
-        
+        recursive(matrix[0], array)
+
     return array
