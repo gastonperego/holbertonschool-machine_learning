d365ab4c5478377c401f4e2692531c16e13e5ea0
gastonperego <gastonperego@gmail.com>
2024-04-12 14:56:26 +0000

    feat: task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Task 14
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        Performs matrix multiplicarion
+    """
+    return np.matmul(mat1, mat2)
