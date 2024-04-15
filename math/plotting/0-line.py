e0eb887008a2f4e7fbef739abdf74ffd420b3aea
gastonperego <gastonperego@gmail.com>
2024-04-15 10:28:35 -0300

    fix: pycodestyle and documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""
+    Task 0
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """
+        Creates a graphic of the number from 0 to 10 to the power of 3
+    """
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
