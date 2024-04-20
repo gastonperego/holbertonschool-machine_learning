f288ed30939d2b6120ea0c62dcefbc50a736ed2e
gastonperego <gastonperego@gmail.com>
2024-04-20 01:32:09 -0300

    feat: task 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Task 10
+"""
+
+
+def poly_derivative(poly):
+    """
+        Calculates the derivative of a polynomial
+    """
+    
+    if len(poly) == 1:
+        return [0]
+    
+    i = 1
+    der = []
+    
+    while i <= len(poly) - 1:
+        der.append((poly[i] * i)) 
+        i += 1
+    
+    return der
+        
+         
