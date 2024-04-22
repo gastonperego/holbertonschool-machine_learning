f7a3c0483de00bb534bc71d267a7a0c3e9cbee33
gastonperego <gastonperego@gmail.com>
2024-04-21 22:42:34 -0300

    fix: functionality

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,14 +6,10 @@
 
 def summation_i_squared(n):
     """
-        Calculates the summation of i^2 with i starting at 1 and stopping at n 
+        Calculates the summation of i^2 with i starting at 1 and stopping at n
     """
-    
+
     if not isinstance(n, int) or n < 1:
         return None
-    
-    if n != 1:
-        return n ** 2 + summation_i_squared(n - 1)
-    
-    return n
-    
+
+    return int((n * (n + 1) * ((2 * n) + 1)) / 6)
