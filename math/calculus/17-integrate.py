b08582fc86ab753f18de2d52ea910f929936a32f
gastonperego <gastonperego@gmail.com>
2024-04-21 23:30:42 -0300

    feat: task 17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Task 17
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+        Calculates the integral of a polynomial
+    """
+
+    integral = [0]
+
+    for i in range(len(poly)):
+        if poly[i] % (i + 1) == 0:
+            integral.append(int(poly[i] / (i + 1)))
+        else:
+            integral.append(poly[i] / (i + 1))
+
+    return integral
