41afdf7193776b3c04cd21f75df6ba041dc56e38
gastonperego <gastonperego@gmail.com>
2024-04-29 16:53:41 -0500

    feat: task 0

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+    Neuron class
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """
+        Neuron class
+    """
+
+    def __init__(self, nx):
+        """
+            Init function
+        """
+        if not isinstance(nx, int):
+            return TypeError('nx must be an integer')
+        elif nx < 1:
+            return ValueError('nx must be a positive integer')
+
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
