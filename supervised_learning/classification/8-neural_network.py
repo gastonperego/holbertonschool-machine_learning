f0ef0a2ea5ea21f0b8cfa33a4f64b62b961de600
gastonperego <gastonperego@gmail.com>
2024-05-03 18:41:43 -0500

    fix: errors

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -15,17 +15,17 @@ class NeuralNetwork:
         """
             Init function
         """
-        if isinstance(nx, int):
+        if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        if isinstance(nodes, int):
+        if not isinstance(nodes, int):
             raise TypeError('nodes must be an integer')
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
-        self.W1 = np.random.normal(size=(1, nx))
-        self.b1 = 0
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-        self.W2 = np.random.normal(size=(1, nx))
+        self.W2 = np.random.normal(size=(1, nodes))
         self.b2 = 0
         self.A2 = 0
