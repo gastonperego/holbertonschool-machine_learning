836320331c0b1ab7c606c3eb9b5d87b38ad88906
gastonperego <gastonperego@gmail.com>
2024-05-07 17:08:24 -0500

    fix: border cases

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -8,9 +8,14 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """
-        converts a numeric label vector into a one-hot matrix
+        Converts a numeric label vector into a one-hot matrix
     """
+    if not isinstance(Y, np.ndarray) or not isinstance(classes, int)\
+            or classes < 2:
+        return None
+    if any([x >= classes for x in Y]):
+        return None
     one_hot = np.zeros((len(Y), classes))
-    for i in range(len(Y)):
+    for i in range(len(Y) - 1):
         one_hot[i][Y[i]] = 1
     return one_hot.T
