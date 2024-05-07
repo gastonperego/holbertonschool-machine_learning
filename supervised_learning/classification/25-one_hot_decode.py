b8f7bdb76137dc94bc38608ae7b125e830aab98d
gastonperego <gastonperego@gmail.com>
2024-05-07 17:41:43 -0500

    fix: border case

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -10,17 +10,16 @@ def one_hot_decode(one_hot):
     """
         Converts a one-hot matrix into a vector of labels
     """
-    try:
-        if not isinstance(one_hot, np.ndarray):
-            return None
-        labels = []
-        for line in one_hot.T:
-            for i in range(len(line)):
-                if line[i] == 0:
-                    continue
-                else:
-                    labels.append(i)
-                    break
-        return np.array(labels)
-    except:
+    if not isinstance(one_hot, np.ndarray):
         return None
+    labels = []
+    for line in one_hot.T:
+        if not isinstance(line, np.ndarray):
+            return None
+        for i in range(len(line)):
+            if line[i] == 0:
+                continue
+            else:
+                labels.append(i)
+                break
+    return np.array(labels)
