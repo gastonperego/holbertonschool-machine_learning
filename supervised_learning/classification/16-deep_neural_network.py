7eb9137bc5f39c98e5e19df46432ced470e36d54
gastonperego <gastonperego@gmail.com>
2024-05-07 12:10:36 -0500

    fix: for loop from conditional removed

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    
+    Deep neural network class
 """
 
 import numpy as np
@@ -8,12 +8,12 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """
-        
+        Deep neural network class
     """
 
     def __init__(self, nx, layers):
         """
-            
+            Init function
         """
 
         if not isinstance(nx, int):
@@ -22,15 +22,15 @@ class DeepNeuralNetwork:
             raise ValueError('nx must be a positive integer')
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
-        if not all(isinstance(n, int) and n > 0 for n in layers):
+        if not all(map(lambda n: isinstance(n, int) and n > 0, layers)):
             raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        i = 0
-        while i < self.L:
-            self.weights[f'W{i + 1}'] = np.random.normal(size=(layers[i],nx))*np.sqrt(2 / layers[i-1])
+        for i in len(self.L):
+            self.weights[f'W{i + 1}'] = np.random.normal(size=(layers[i], nx)
+                                                         )*np.sqrt(2 / layers[
+                                                             i-1])
             self.weights[f'b{i + 1}'] = np.zeros((layers[i], 1))
             nx = layers[i]
-            i += 1
