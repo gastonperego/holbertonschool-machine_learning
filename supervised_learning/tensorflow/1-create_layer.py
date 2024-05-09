f35688ee6555731cd839f981be812fa523b39d06
gastonperego <gastonperego@gmail.com>
2024-05-09 08:00:05 -0500

    feat: task 1

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Create placeholders function
+"""
+
+import tensorflow
+tf = tensorflow.compat.v1
+tf.disable_eager_execution()
+
+
+def create_layer(prev, n, activation):
+    """
+        Creates a layer of the neural network
+    """
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(units=n, kernel_initializer=initializer,
+                                  activation=activation, name='layer')
+
+    return layer(prev)
