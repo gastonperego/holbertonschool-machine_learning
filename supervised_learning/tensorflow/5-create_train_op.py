772a31ec5a2a125846aa22ba770b260006e5da10
gastonperego <gastonperego@gmail.com>
2024-05-11 16:18:01 -0500

    feat: task 5

---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Create train op function
+"""
+
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+
+
+def create_train_op(loss, alpha):
+    """
+        Creates the training operation for the network
+    """
+
+    optimizer = tf.train.GradientDescentOptimizer(alpha)
+    train = optimizer.minimize(loss)
+
+    return train
+ 
