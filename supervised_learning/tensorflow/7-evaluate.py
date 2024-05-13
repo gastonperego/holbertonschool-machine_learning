c9379b2b9eca49d0a84e55072861de77740178da
gastonperego <gastonperego@gmail.com>
2024-05-12 20:57:22 -0500

    feat: task 7

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+    Evaluate function
+"""
+
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+
+
+def evaluate(X, Y, save_path):
+    """
+        Evaluates the output of a neural network
+    """
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + ".meta")
+        saver.restore(sess, save_path)
+
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        
+        eval = sess.run(pred, feed_dict={x: X, y: Y})
+        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss = sess.run(loss, feed_dict={x: X, y: Y})
+        return eval, accuracy, loss
