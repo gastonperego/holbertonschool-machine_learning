a5ad223d83eadbb6f97065428c5ca180a11101d5
gastonperego <gastonperego@gmail.com>
2024-05-12 19:44:21 -0500

    fix: extra print

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -28,7 +28,7 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     sess = tf.Session()
     sess.run(init)
     for i in range(iterations + 1):
-        if i % 100 == 0 or i == 0 or:
+        if i % 100 == 0 or i == 0:
             t_loss = sess.run(loss, feed_dict={x: X_train, y: Y_train})
             t_acc = sess.run(acc, feed_dict={x: X_train, y: Y_train})
             v_loss = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
