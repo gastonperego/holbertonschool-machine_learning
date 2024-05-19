6d89f5361c95a541de733bc2ef12bff8bceebb83
gastonperego <gastonperego@gmail.com>
2024-05-19 15:48:13 -0500

    feat: task 10

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Update_variables_momentum function
+"""
+
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """
+        Sets up the Adam optimization algorithm in TensorFlow
+    """
+    return tf.keras.optimizers.Adam(learning_rate=alpha,
+                                    beta_1=beta1, beta_2=beta2,
+                                    epsilon=epsilon)
