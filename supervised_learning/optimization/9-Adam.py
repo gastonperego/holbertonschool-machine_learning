9cf6b25f54740d11da80b3d4632d59b508c29389
gastonperego <gastonperego@gmail.com>
2024-05-19 18:23:33 -0500

    feat: not using updated values

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """
-    Create_mini_batches function
+    Update_variables_Adam function
 """
 
 import numpy as np
-
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
@@ -15,9 +14,10 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     new_first = beta1 * v + (1 - beta1) * grad
     new_second = beta2 * s + (1 - beta2) * (grad ** 2)
 
-    v_corrected = v / (1 - beta1 ** t)
-    s_corrected = s / (1 - beta2 ** t)
+    v_corrected = new_first / (1 - beta1 ** t)
+    s_corrected = new_second / (1 - beta2 ** t)
 
-    updated_var = var - alpha * v_corrected / (np.sqrt(s_corrected) + epsilon)
+    updated_var = var - (alpha * (v_corrected /
+                                  (np.sqrt(s_corrected) + epsilon)))
 
     return updated_var, new_first, new_second
