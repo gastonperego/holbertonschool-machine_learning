91618092540b4cc153775aeadce220f2b352cf70
gastonperego <gastonperego@gmail.com>
2024-05-20 18:24:41 -0500

    feat: task 1

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Sensitivity function
+"""
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """
+        Calculates the sensitivity of each class of a confusion matrix
+    """
+    sensitivity = []
+    for i in range(confusion.shape[0]):
+        sensitivity.append(confusion[i][i] / confusion[i].sum())
+
+    return sensitivity
