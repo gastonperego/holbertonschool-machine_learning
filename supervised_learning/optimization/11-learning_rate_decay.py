97048a19cfe486db07adb28b49cbf2dad9a3878e
gastonperego <gastonperego@gmail.com>
2024-05-19 21:10:33 -0500

    feat: task 11

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Learning_rate_decay function
+"""
+
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+        Updates the learning rate using inverse time decay in numpy
+    """
+    
+
+    updated_alpha = (alpha / (1 + decay_rate * int(global_step / decay_step))) 
+
+    return updated_alpha
