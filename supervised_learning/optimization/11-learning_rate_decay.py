2010d221d8cf797b736d994825f683ddbdf6013f
gastonperego <gastonperego@gmail.com>
2024-05-19 21:11:32 -0500

    fix: pycodestyle

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -10,8 +10,7 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
         Updates the learning rate using inverse time decay in numpy
     """
-    
 
-    updated_alpha = (alpha / (1 + decay_rate * int(global_step / decay_step))) 
+    updated_alpha = (alpha / (1 + decay_rate * int(global_step / decay_step)))
 
     return updated_alpha
