afd58899b92402d0fcb0913a2786717e04bd8716
gastonperego <gastonperego@gmail.com>
2024-05-25 17:36:43 -0500

    feat: task 2

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    l2 reg cost function
+"""
+
+import numpy as np
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    """
+        Calculates the cost of a neural network with L2 regularization
+    """
+    tensor = tf.stack(model.losses) + cost
+
+    return tensor
