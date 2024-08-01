214b5636a2c8aa329c647591cabdbb0cf2a02f29
gastonperego <gastonperego@gmail.com>
2024-08-01 16:57:21 -0500

    feat: task 2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    matrix_shape: Calculates the shape of a matrix
+"""
+def matrix_shape(matrix):
+    
+    shape = []
+    while (not isinstance(matrix, int)):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return tuple(shape)
