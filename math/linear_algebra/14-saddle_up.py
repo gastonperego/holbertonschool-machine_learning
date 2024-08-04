191dcf340261868cf28202fd430339680c75e210
gastonperego <gastonperego@gmail.com>
2024-08-04 16:28:16 -0300

    faet: task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    np_matmul: Performs matrix multipliaction between the 2 given matrices
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        Returns a np.nsarray that is the product of the 2 matrices
+    """
+    return np.matmul(mat1, mat2)
