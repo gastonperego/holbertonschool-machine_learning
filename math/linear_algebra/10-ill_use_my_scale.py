35c0a5bf2cc26e55077928d079ce94887ad15a09
gastonperego <gastonperego@gmail.com>
2024-08-04 16:07:10 -0300

    feat: task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    np_shape: Calculates the shape of a numpy.ndarray
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+        Returns the shape of a np.ndarray as a tuple of ints
+    """
+    return matrix.shape
