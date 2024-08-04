9336de4303e7230055d1a688a5cd94deb07953f0
gastonperego <gastonperego@gmail.com>
2024-08-04 16:21:32 -0300

    fix: new array returned

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -10,4 +10,5 @@ def np_cat(mat1, mat2, axis=0):
         Returns a new np.ndarray that is the concatenations of the 2 matrices
         along the specified axis
     """
-    return np.concat([mat1, mat2], axis)
+    array = np.concat([mat1, mat2], axis) 
+    return array
