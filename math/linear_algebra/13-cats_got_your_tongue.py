1dd1579d6ab987565746c5f97abce1994901e194
gastonperego <gastonperego@gmail.com>
2024-08-04 16:19:51 -0300

    feat: task 13

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    np_cat: Concatenates 2 matries along a specific axis
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+        Returns a new np.ndarray that is the concatenations of the 2 matrices
+        along the specified axis
+    """
+    return np.concat([mat1, mat2], axis)
