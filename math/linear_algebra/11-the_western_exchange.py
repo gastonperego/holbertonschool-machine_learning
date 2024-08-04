9e0dcbf1070781a60044ec8c8b6d9472b9161620
gastonperego <gastonperego@gmail.com>
2024-08-04 16:10:40 -0300

    feat:t ask 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    np_transpose: Transposes a matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+        Returns a np.ndarray that is the traspose of matrix
+    """
+    return matrix.T
