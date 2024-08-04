ee46d8e3f100730bf21effd8f939dd0a06568ba9
gastonperego <gastonperego@gmail.com>
2024-08-04 10:52:39 -0300

    feat: task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    cat_arrays: Concatenates 2 arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+        Returns a new array that is arr1 + arr2
+    """
+    return arr1 + arr2
