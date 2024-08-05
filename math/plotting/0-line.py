a2cfefaf5cab6fdc5da39a185f382abd91e684bd
gastonperego <gastonperego@gmail.com>
2024-08-05 13:06:38 -0300

    feat: task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""
+    line: Plots a linaear function
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """
+        Plots a linear function
+    """
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
