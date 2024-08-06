b6c019edd3de0b691da55539d78b37ffc61aab2d
gastonperego <gastonperego@gmail.com>
2024-08-06 09:59:38 -0300

    feat: task 6

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+    bars: Plots a bar graph
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """
+        Plots a bar graph that contains the numer of fruits that each person
+        has
+    """
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.ylim((0, 80))
+    plt.ylabel('Quantity of Fruit')
+
+    plt.bar(['Farrah', 'Fred', 'Felicia'], fruit[0], color='r',
+            label='apples', width=0.5)
+    plt.bar(['Farrah', 'Fred', 'Felicia'], fruit[1], bottom=fruit[0],
+            color='y', label='bananas', width=0.5)
+    plt.bar(['Farrah', 'Fred', 'Felicia'], fruit[2], bottom=fruit[0]
+            + fruit[1], color='#ff8000', label='oranges', width=0.5)
+    plt.bar(['Farrah', 'Fred', 'Felicia'], fruit[3], bottom=fruit[0]
+            + fruit[1] + fruit[2], color='#ffe5b4', label='peaches', width=0.5)
+    plt.legend()
+    plt.suptitle('Number of Fruit per Person')
+
+    plt.show()
