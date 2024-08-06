01f7cf09f13010eeffbba9b2e9e4e3da75d33b2d
gastonperego <gastonperego@gmail.com>
2024-08-06 10:09:15 -0300

    fix:? ticks added?

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -15,8 +15,10 @@ def bars():
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.ylim((0, 80))
     plt.ylabel('Quantity of Fruit')
+    plt.ylim((0, 80))
+    plt.yticks([0, 10, 20, 30, 40, 50, 60, 70, 80])
+    plt.suptitle('Number of Fruit per Person')
 
     plt.bar(['Farrah', 'Fred', 'Felicia'], fruit[0], color='r',
             label='apples', width=0.5)
@@ -27,6 +29,5 @@ def bars():
     plt.bar(['Farrah', 'Fred', 'Felicia'], fruit[3], bottom=fruit[0]
             + fruit[1] + fruit[2], color='#ffe5b4', label='peaches', width=0.5)
     plt.legend()
-    plt.suptitle('Number of Fruit per Person')
-
+    plt.savefig('plot.png')
     plt.show()
