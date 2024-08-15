27b381f7047edca0cc62e6e0626408e194607fdd
gastonperego <gastonperego@gmail.com>
2024-08-15 11:42:00 -0300

    fix: pycodestyle and documentation

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -16,6 +16,9 @@ import numpy as np
 
 
 class Node:
+    """
+        Node class
+    """
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
         """
@@ -43,6 +46,9 @@ class Node:
 
 
 class Leaf(Node):
+    """
+        Leaf class
+    """
     def __init__(self, value, depth=None):
         """
             Init function
@@ -60,6 +66,9 @@ class Leaf(Node):
 
 
 class Decision_Tree():
+    """
+        Decision_Tree class
+    """
     def __init__(self, max_depth=10, min_pop=1,
                  seed=0, split_criterion="random", root=None):
         """
